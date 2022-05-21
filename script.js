8a62db9134d93ad9543dd33a0947ceb9fb3ebfe4
JD <upkanet@gmail.com>
2022-05-21 14:48:29 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// let btns = document.querySelectorAll('[data-href]');
+// btns.forEach(btn => {
+//     btn.addEventListener('click',(e)=>{
+//         location.href = btn.getAttribute("data-href");
+//     })
+// })
