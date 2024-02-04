aecfba0c86be664b032397d03f5e52070e2bb5cd
upkanet <upkanet@gmail.com>
2024-02-04 10:25:19 +0100

    refactor:moved to legacy

--- script.js
+++
@@ -1,6 +0,0 @@
-// let btns = document.querySelectorAll('[data-href]');
-// btns.forEach(btn => {
-//     btn.addEventListener('click',(e)=>{
-//         location.href = btn.getAttribute("data-href");
-//     })
-// })
