72f6bf451728914af4de3da79f99a144e09177b3
upkanet <upkanet@gmail.com>
2026-09-02 23:19:38 +0200

    port

--- server.mjs
+++ server.mjs
@@ -2,7 +2,7 @@ import express from 'express'
 import fs from 'fs'
 import {default as showdown}  from 'showdown'
 const app = express()
-const port = 3000
+const port = 3002
 
 import listContents from 'list-contents'
 
